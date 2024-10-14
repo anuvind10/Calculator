39782eb2aa10df299cb9b5db614e18ee3822a15b
Anuvind <119839716+anuvind10@users.noreply.github.com>
2024-10-14 19:24:48 +0530

    initial commit with basic structure for the calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+function addNumbers(num1, num2) {
+    return  num1+num2;
+}
+
+function subtractNumbers(num1, num2) {
+    return num1-num2;
+}
+
+function multiplyNumbers(num1, num2) {
+    return num1*num2;
+}
+
+function divideNumbers(num1, num2) {
+    return num1/num2;
+}
+
+function operate(operator, num1, num2){
+    
+}
+
