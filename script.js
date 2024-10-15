0935f0327ba98479767b1ba14bfebd712ae828ab
Anuvind <119839716+anuvind10@users.noreply.github.com>
2024-10-15 22:50:19 +0530

    working display

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const numbers = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.operator');
+const display = document.querySelector('#display');
+const clear = document.querySelector("#clear");
+const equals = document.querySelector("#equals");
+let numberArray =[];
+let operatorArray = [];
 
 function addNumbers(num1, num2) {
     return  num1+num2;
@@ -16,23 +23,48 @@ function divideNumbers(num1, num2) {
 }
 
 function operate(operator, num1, num2){
+    let displayValue
+    switch(operator){
+        case "+":
+            displayValue = addNumbers(num1, num2);
+            break;
+            
+        case "-":
+            displayValue = subtractNumbers(num1, num2);
+            break;
+        
+        case "x":
+            displayValue = multiplyNumbers(num1, num2);
+            break;
 
+        case "/":
+            displayValue = divideNumbers(num1, num2);
+    }
+    display.value = displayValue;
 }
 
-const numbers = document.querySelectorAll('.num');
-const operators = document.querySelectorAll('.operator');
-const display = document.querySelector('#display')
-
-function populateDisplay() {
-    const clickedValue = this.textContent;
-    let number = "";
-    display.textContent = clickedValue;
-    
-    if (this.classList == "num") {
-        number = clickedValue;
+function populateDisplay(input) {
+    const clickedValue = input.textContent;
+    if (input.id != "equals" && input.id != "clear") {
+        display.value += clickedValue;
     }
 }
 
-numbers.forEach((number) => number.addEventListener('click', populateDisplay))
-operators.forEach((operator) => operator.addEventListener('click', populateDisplay))
+function clearDisplay() {
+    display.value = "";
+    numberArray = [];
+    operatorArray = [];
+}
 
+let arr=[];
+
+// equals.addEventListener('click', operate(operator, num1, num2));
+clear.addEventListener('click',clearDisplay);
+numbers.forEach((number) => number.addEventListener('click', () => {
+    numberArray.push(Number(number.textContent));
+    populateDisplay(number);
+}));
+operators.forEach((operator) => operator.addEventListener('click', () => {
+    operatorArray.push(operator.textContent);
+    populateDisplay(operator);
+}));
